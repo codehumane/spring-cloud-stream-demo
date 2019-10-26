35d9bb8cdc86a3908357617a32eda368686000d3
codehumane <codehumane@gmail.com>
2019-10-26 20:49:39 +0900

    Spring Cloud Function 방식으로 변경

--- src/main/kotlin/codehumane/springcloudstreamdemo/SpringCloudStreamDemoApplication.kt
+++ src/main/kotlin/codehumane/springcloudstreamdemo/SpringCloudStreamDemoApplication.kt
@@ -3,9 +3,10 @@ package codehumane.springcloudstreamdemo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.stream.annotation.EnableBinding
-import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.cloud.stream.messaging.Sink
-import org.springframework.stereotype.Service
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.function.Consumer
 
 @SpringBootApplication
 @EnableBinding(Sink::class)
@@ -15,12 +16,12 @@ fun main(args: Array<String>) {
     runApplication<SpringCloudStreamDemoApplication>(*args)
 }
 
-@Service
-class StreamApplicationService {
+@Configuration
+class StreamListenerConfig {
 
-    @StreamListener(Sink.INPUT)
-    fun handle(message: String) {
-        println("Received: $message")
+    @Bean
+    fun loggingConsumer(): Consumer<String> = Consumer {
+        println("Received: $it")
     }
 
 }
